48f4f6ff5aafbdbd20accf02710a5379bec34621
Alex Klyuev <aklyuev96@gmail.com>
2021-05-05 15:08:15 -0700

    create App component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-const App = () => (
-  <h1>App</h1>
-);
+import React from "react";
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    // This component will manage modal state in case user clicks out fo the workflow
+    this.state = {
+      modal: 0
+    };
+  }
+
+  render() {
+    return (
+      <h1>App</h1>
+    );
+  }
+};
 
 export default App;
