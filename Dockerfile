e534c7c04464bb110010fbdf2755721d1e867c2d
Alex Klyuev <aklyuev96@gmail.com>
2021-05-10 13:58:27 -0700

    deployment successful

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:latest
-WORKDIR /src/app
-COPY . /src/app
-RUN npm install
-RUN npm run build
-EXPOSE 3000
-CMD ["npm", "start"]
--- next.config.js
+++
@@ -1,6 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js' : '',
-}
